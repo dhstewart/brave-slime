eb429c35c2fb2f326d6110580fb309cb520c6ce5
Hunter Stewart <hetre85@gmail.com>
2014-08-29 11:18:15 -0500

    Organized and refactored bg, added more enemies, small blocks

--- Assets/Scripts/SlimeHealth.cs
+++ Assets/Scripts/SlimeHealth.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class SlimeHealth : MonoBehaviour {
 
+	float dam = 1f;
+	public float hp = 3;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +13,19 @@ public class SlimeHealth : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void TakeDamage (float dam)
+	{
+		hp -= dam;
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		
+		if(col.gameObject.tag == "enemy")
+		{
+			TakeDamage (dam);
+		}
 	}
 }
--- Assets/Scripts/redSlimeHealth.cs
+++ Assets/Scripts/redSlimeHealth.cs
@@ -2,8 +2,7 @@
 using System.Collections;
 
 public class redSlimeHealth : MonoBehaviour {
-
-
+	
 	public float hp = 15f;
 	// Use this for initialization
 	void Start () {
