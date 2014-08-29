d567df2c826912167575ab0ee9cd3e7822cc84f6
Hunter Stewart <hetre85@gmail.com>
2014-08-29 12:35:00 -0500

    Added objecte references to get heart1 to remove itself upon enemy collision

--- Assets/Scripts/SlimeHealth.cs
+++ Assets/Scripts/SlimeHealth.cs
@@ -3,29 +3,31 @@ using System.Collections;
 
 public class SlimeHealth : MonoBehaviour {
 
-	float dam = 1f;
-	public float hp = 3;
+
+	public Heart1 heart1;
+
 	// Use this for initialization
+
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	public void TakeDamage (float dam)
+	void Update ()
 	{
-		hp -= dam;
+
 	}
+	
 
 	void OnCollisionEnter2D (Collision2D col)
 	{
 		
 		if(col.gameObject.tag == "enemy")
 		{
-			TakeDamage (dam);
+			if(heart1 != null)
+			{
+				heart1.RemoveHeart();
+			}
 		}
 	}
 }
